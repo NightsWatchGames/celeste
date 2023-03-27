6a46b33f2c3faabe6e07da6a37a6d57333ed1c8c
littlekite <15829705867@sina.cn>
2023-03-27 18:59:10 +0800

    窗口设置

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,6 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin {
             default_sampler: ImageSampler::nearest_descriptor(),
-        }).set(WindowPlugin { //设置窗口大小 1100*750
-            primary_window: Some(Window{
-                position:WindowPosition::Centered(MonitorSelection::Primary),//窗口居中
-                resolution: WindowResolution::new(1100.0,750.0),
-                ..default()
-            }),
-            ..default()
         }))
         .add_plugin(WorldInspectorPlugin::new())
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
@@ -53,14 +46,4 @@ pub fn setup_camera(mut commands: Commands) {
     let mut camera2d_bundle = Camera2dBundle::default();
     camera2d_bundle.projection.scale = CAMERA_SCALE;
     commands.spawn(camera2d_bundle);
-<<<<<<< HEAD
-}
-
-pub fn setup_ldtk_world(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(LdtkWorldBundle {
-        ldtk_handle: asset_server.load("test.ldtk"),
-        ..Default::default()
-    });
-=======
->>>>>>> a65fc5d4f1dbd758c8da417f87234cc365467937
 }
