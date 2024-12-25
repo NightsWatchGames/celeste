0bf57a161ff5848b3b0c61dfb197d61623cdbeff
张林伟 <lewiszlw520@gmail.com>
2024-12-25 14:08:52 +0800

    Fix gravity scale

--- src/common.rs
+++ src/common.rs
@@ -2,7 +2,7 @@ use bevy::prelude::*;
 
 pub const CAMERA_SCALE: f32 = 0.25;
 pub const TILE_SIZE: f32 = 8.0;
-pub const PLAYER_GRAVITY_SCALE: f32 = 10.0;
+pub const PLAYER_GRAVITY_SCALE: f32 = 1.0;
 pub const PLAYER_DASHING_COLOR: Color = Color::srgb(
     41 as f32 / u8::MAX as f32,
     173 as f32 / u8::MAX as f32,
